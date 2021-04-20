caa96d08a6f776cacb71c01343fee1f57c5ea6f8
programming-enthusiast <jimmy.lin.dev@gmail.com>
2021-04-20 14:31:07 +0800

    Fix minor issues

--- src/components/layout/sidebar/Sidebar.js
+++ src/components/layout/sidebar/Sidebar.js
@@ -1,28 +1,39 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
+import { ReactComponent as RoomsIcon } from '../../../assets/svg/rooms.svg'
 import { ReactComponent as DailyScheduleIcon } from '../../../assets/svg/daily_schedule.svg'
+import { ReactComponent as MonthlyScheduleIcon } from '../../../assets/svg/monthly_schedule.svg'
 
 export default function Sidebar() {
+  const location = useLocation()
+  const history = useHistory()
+  
+  const handleDailyScheduleClick = () => {
+    history.push('/daily_schedule')
+  }
+
+  const handleMonthlyScheduleClick = () => {
+    history.push('/monthly_schedule')
+  }
+
+  const handleHomeClick = () => {
+    history.push('/')
+  }
+
   return (
     <div className="sidebar__container">
-      <Link to="/">
-        <div className="sidebar__item-container">
-          <div className="sidebar__item-icon"><DailyScheduleIcon fill="white"/></div>
-          <div>Home</div>
-        </div>
-      </Link>
-      <Link to="/monthly_schedule">
-        <div className="sidebar__item-container">
-          <div className="sidebar__item-icon"><DailyScheduleIcon fill="white"/></div>
-          <div>Monthly Schedule</div>
-        </div>
-      </Link>
-      <Link to="/daily_schedule">
-        <div className="sidebar__item-container">
-          <div className="sidebar__item-icon"><DailyScheduleIcon fill="white"/></div>
-          <div>Daily Schedule</div>
-        </div>
-      </Link>
+      <div className={`sidebar__item-container ${location.pathname === '/' ? 'selected' : ''}`} onClick={handleHomeClick}>
+        <div className="sidebar__item-icon"><RoomsIcon fill="white"/></div>
+        <div>Rooms</div>
+      </div>
+      <div className={`sidebar__item-container ${location.pathname === '/monthly_schedule' ? 'selected' : ''}`} onClick={handleMonthlyScheduleClick}>
+        <div className="sidebar__item-icon"><MonthlyScheduleIcon fill="white"/></div>
+        <div>Monthly Schedule</div>
+      </div>
+      <div className={`sidebar__item-container ${location.pathname.includes('/daily_schedule') ? 'selected' : ''}`} onClick={handleDailyScheduleClick}>
+        <div className="sidebar__item-icon"><DailyScheduleIcon fill="white"/></div>
+        <div>Daily Schedule</div>
+      </div>
     </div>
   )
 }
